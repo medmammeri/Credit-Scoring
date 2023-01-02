63a02c93ed76fb329dc8e297abd519de2477cbc9
Mohamed Rafik Mammeri <mammeri.mohamed@gmail.com>
2023-01-02 19:50:27 +0100

    update

--- creditscoring/modelling/metrics.py
+++ creditscoring/modelling/metrics.py
@@ -19,3 +19,21 @@ def gini(y_true, y_score, pos_label=None):
 
 def scorer():
     return make_scorer(gini, needs_proba=True)
+
+
+def hello(name: str) -> str:
+    """Just an greetings example.
+
+    Args:
+        name (str): Name to greet.
+
+    Returns:
+        str: greeting message
+
+    Examples:
+        .. code:: python
+
+            >>> hello("Roman")
+            'Hello Roman!'
+    """
+    return f"Hello {name}!"
